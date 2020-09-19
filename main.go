93415ce3be9ff02be9330659d948c18a2e30a449
Luqmanul Hakim <arlhba@gmail.com>
2020-09-20 04:37:02 +0700

    add file rotation logic and config | add max video length config

--- main.go
+++ main.go
@@ -6,7 +6,12 @@ import (
 	"os"
 	"os/exec"
 	"os/signal"
+	"path/filepath"
+	"regexp"
+	"strconv"
+	"strings"
 	"syscall"
+	"time"
 
 	"github.com/spf13/viper"
 )
@@ -22,14 +27,25 @@ type Camera struct {
 func (c *Camera) Start() error { return c.CMD.Start() }
 
 // Stop ...
-func (c *Camera) Stop() error { return c.CMD.Wait() }
+func (c *Camera) Stop() error {
+	if err := c.CMD.Process.Signal(syscall.SIGTERM); err != nil {
+		log.Println("camera: ", err.Error())
+	}
+	return c.CMD.Wait()
+}
 
 // Config ...
 type Config struct {
-	Cameras []*Camera `mapstructure:"cameras"`
+	RetentionFile  float64   `mapstructure:"retention_file"`
+	MaxVideoLength string    `mapstructure:"max_video_length"`
+	Cameras        []*Camera `mapstructure:"cameras"`
 }
 
-var config Config
+var (
+	config          Config
+	cwd, _          = os.Getwd()
+	getTimestamp, _ = regexp.Compile(".*?_(.*?)[.]")
+)
 
 func initConfig() {
 	viper.SetConfigName("config")
@@ -47,6 +63,7 @@ func initConfig() {
 func main() {
 
 	initConfig()
+	checkOrCreateDataDir()
 
 	sigs := make(chan os.Signal, 1)
 	done := make(chan bool, 1)
@@ -58,21 +75,86 @@ func main() {
 		done <- true
 	}()
 
-	for i, c := range config.Cameras {
-		filename := fmt.Sprintf("%s.mp4", c.Name)
-		config.Cameras[i].CMD = exec.Command("ffmpeg", "-i", c.Server, "-acodec", "aac", "-vcodec", "copy", "-f", "mp4", "-y", filename)
-		cwd, _ := os.Getwd()
-		config.Cameras[i].CMD.Dir = cwd
+	ticker := time.NewTicker(viper.GetDuration("max_video_length"))
+	quitTicker := make(chan struct{})
 
+	startAllCamera()
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				stopAllCamera()
+				startAllCamera()
+				fileRotation()
+				// do stuff
+			case <-quitTicker:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+	<-done
+	quitTicker <- struct{}{}
+	stopAllCamera()
+}
+
+func startAllCamera() {
+	for i, c := range config.Cameras {
+		t := time.Now()
+		filename := fmt.Sprintf("%s/data/%s_%d.mp4", cwd, c.Name, t.Unix())
+		config.Cameras[i].CMD = exec.Command("ffmpeg", "-i", c.Server, "-acodec", "aac", "-vcodec", "copy", "-f", "mp4", "-y", filename)
+		config.Cameras[i].CMD.Dir = cwd
 		if err := c.Start(); err != nil {
 			log.Println("camera: " + err.Error())
 		}
 	}
+}
 
-	<-done
+func stopAllCamera() {
 	for _, c := range config.Cameras {
-		if err := c.Stop(); err != nil {
-			log.Println("camera: " + err.Error())
+		if c.CMD != nil {
+			if err := c.Stop(); err != nil {
+				log.Println("camera: " + err.Error())
+			}
+		}
+	}
+}
+
+func checkOrCreateDataDir() {
+	err := os.Mkdir("data", 0755)
+	if err != nil && err.Error() != "mkdir data: file exists" {
+		panic(err)
+	}
+}
+
+func fileRotation() {
+	var files []string
+
+	root := cwd + "/data"
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		files = append(files, path)
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range files {
+		ss := strings.Split(file, "_")
+		if len(ss) < 2 {
+			continue
+		}
+
+		ss = strings.Split(ss[1], ".")
+
+		tu, _ := strconv.Atoi(ss[0])
+		tf := time.Unix(int64(tu), 0)
+		tn := time.Now()
+		d := tn.Sub(tf)
+
+		if d.Hours() > config.RetentionFile {
+			if err := os.Remove(file); err != nil {
+				log.Println("file rotation: " + err.Error())
+			}
 		}
 	}
 }
--- story
+++
@@ -1,6 +0,0 @@
-TODO:
-- as a user i want this service have retention file period for 2 days and configurable
-- as a user i want spesify location to store data
-
-DONE:
-- as a user i want this service can add as many camera name and rtsp with config
